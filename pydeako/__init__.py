041c6178878c71b23ec3728e5112d521c74a07ed
Blake Bryant <goos@deako.com>
2023-08-29 23:29:51 +0000

    ci and docker file

---
+++ pydeako/__init__.py
@@ -0,0 +1,6 @@
+"""
+pydeako module provides the following:
+ - models to interact with Deako devices locally with a socket connection
+ - implementation of a Deako socket client
+ - mdns discovery client
+"""
